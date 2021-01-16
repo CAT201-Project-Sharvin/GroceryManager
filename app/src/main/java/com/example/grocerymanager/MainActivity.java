c348505f8618c0c3a505f0ab141007ac6a05193f
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2021-01-17 03:56:36 +0800

    Activated Notification

--- app/src/main/java/com/example/grocerymanager/MainActivity.java
+++ app/src/main/java/com/example/grocerymanager/MainActivity.java
@@ -17,6 +17,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
