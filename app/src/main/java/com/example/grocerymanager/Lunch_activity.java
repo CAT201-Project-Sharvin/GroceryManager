396270ada54ba2f13d80629f926d9285893de48a
Pveen3113 <praveen@student.usm.my>
2021-01-17 13:12:03 +0800

    Pathway to recipe and list grocery  from notification is updated

--- app/src/main/java/com/example/grocerymanager/Lunch_activity.java
+++ app/src/main/java/com/example/grocerymanager/Lunch_activity.java
@@ -9,6 +9,6 @@ public class Lunch_activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lunch_activity_layout);
+        setContentView(R.layout.activity_recipe);
     }
 }
