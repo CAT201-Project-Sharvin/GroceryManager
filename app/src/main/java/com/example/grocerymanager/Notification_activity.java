307cded3ac0ff25e7526a546878e00cbdee705b8
Pveen3113 <praveen@student.usm.my>
2021-01-17 15:58:58 +0800

    Redundant class Deleted

--- app/src/main/java/com/example/grocerymanager/Notification_activity.java
+++ app/src/main/java/com/example/grocerymanager/Notification_activity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Breakfast_activity extends AppCompatActivity {
+public class Notification_activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
