3ec93fa6c4363af7cb0325f09a884a1f438c2e9d
Pveen3113 <praveen@student.usm.my>
2021-01-17 20:21:56 +0800

    Logo updated , Comment prompts are added in the code, Redundant classes are further filtered

--- app/src/main/java/com/example/grocerymanager/Notification_receiver_Dinner.java
+++ app/src/main/java/com/example/grocerymanager/Notification_receiver_Dinner.java
@@ -1,6 +1,6 @@
 package com.example.grocerymanager;
 
-import android.app.NotificationManager;
+
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -16,6 +16,7 @@ public class Notification_receiver_Dinner extends BroadcastReceiver {
         repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,300,repeating_intent,PendingIntent.FLAG_UPDATE_CURRENT);
 
+        //Builds the notification while in sync with the notification channel via the similar channel id
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"dinner")
                 .setContentIntent(pendingIntent)
                 .setSmallIcon(android.R.drawable.ic_notification_overlay)
@@ -25,7 +26,7 @@ public class Notification_receiver_Dinner extends BroadcastReceiver {
                 .setAutoCancel(true);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-
+        //unique id to link with the channel to determine which notification this is suppose to be
         notificationManager.notify(300,builder.build());
     }
 }
